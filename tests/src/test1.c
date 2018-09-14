2783e2e6868646f9b7d5aa93135e35b01a132422
Elizabeth KELEN <ekelen@e1r4p13.42.fr>
2018-09-14 11:56:52 +0200

    ammend correct output

--- tests/src/test1.c
+++ tests/src/test1.c
@@ -13,3 +13,19 @@ int      main()
    }
    return (0);
 }
+
+// 0.00 real 0.00 user 0.00 sys
+//        1544192 maximum resident set size
+//            0 average shared memory size
+//            0 average unshared data size
+//            0 average unshared stack size
+//           396 page reclaims
+//            0 page faults
+//            0 swaps
+//            0 block input operations
+//            0 block output operations
+//            0 messages sent
+//            0 messages received
+//            0 signals received
+//            0 voluntary context switches
+//            1 involuntary context switches
