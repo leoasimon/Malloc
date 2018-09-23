566a410b9fc9e39bdb9a37a41d9bb475b7379968
Elizabeth Kelen <elizabethkelen@liz.home>
2018-09-23 11:30:25 +0200

    tests

---
+++ test5.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+int main()
+{
+   malloc(1024);
+   malloc(1024 * 32);
+   malloc(1024 * 1024);
+   malloc(1024 * 1024 * 16);
+   malloc(1024 * 1024 * 128);
+   show_alloc_mem();
+   return (0);
+}
+// $&gt; gcc -o test5 test5.c -L. -lft_malloc
+// $&gt; ./test5
+
+// L'affichage correspond au sujet et à la répartition TINY/SMALL/LARGE du projet ?
